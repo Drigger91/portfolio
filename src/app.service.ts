74c65033d699ffe05f62afe52948ec4a53260589
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2022-11-15 20:54:41 +0530

    Initialised nestjs project

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello  Nest!';
+  }
+  GreetInTs() : string{
+    return "Greetings";
+  }
+}
