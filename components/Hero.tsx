05e02d83b8b813f70c1913aa42c25533ba574960
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2023-04-16 00:51:44 +0530

    minor ui changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,10 +32,10 @@ const Hero = ({ mainData }: HeroProps) => {
 
                 <div className="flex flex-col gap-4 md:gap-6 text-left lg:w-1/2 2xl:w-1/3 mx-4 md:mx-6 xl:mx-0">
                     <div className="flex items-center gap-1">
+                    <strong className="text-lg md:text-xl mt-2 md:mt-1.5">
+                            Hello
+                        </strong>
                         <Image unoptimized={true} alt='waving-hand' width={30} height={30} src={wavingHand} />
-                        <p className="text-lg md:text-xl mt-2 md:mt-1.5">
-                            Hey
-                        </p>
                     </div>
                     <h1 className="text-4xl md:text-6xl font-bold relative">
                         I&apos;m {name}
@@ -61,10 +61,6 @@ const Hero = ({ mainData }: HeroProps) => {
                         {shortDesc}
                     </p>
 
-                    {/* <a href="https://sppuprep.tech" target="_blank" rel="noopener noreferrer" className="relative whitespace-nowrap before:absolute before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] hover:before:scale-y-100 before:transition-transform before:ease-in-out before:duration-500 before:bg-violet-300 dark:before:bg-violet-600">
-                        <span className="relative">SPPU Prep</span>
-                    </a> */}
-
                     <ScrollLink
                         className="w-fit text-sm md:text-base py-2 px-4 cursor-pointer flex items-center gap-1 rounded-md bg-violet-600 hover:bg-violet-700 dark:bg-violet-700 hover:dark:bg-violet-800 transition-colors group text-white"
                         to={'about'}
@@ -80,7 +76,7 @@ const Hero = ({ mainData }: HeroProps) => {
 
                 <div className="relative mx-auto lg:mx-0 mt-12 md:mt-16 lg:mt-0">
                     <div className="w-56 h-56 md:w-80 md:h-80 lg:-translate-x-16">
-                        <Image alt='avatar' width={1000} height={1000} className="rounded-full w-full h-full object-cover" src={heroImage} />
+                        <Image alt='avatar' width={1000} height={1000} className="rounded-full w-full h-full object-cover" src ="/heroImage.jpg" />
                     </div>
 
                     <div className="absolute grid -top-6 -left-12 lg:-top-14 lg:-left-32 w-16 h-16 md:w-20 md:h-20 bg-white dark:bg-grey-800 rounded-full place-items-center hover:shadow-lg transition-shadow">
