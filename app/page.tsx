5b794b8d0cb2c97ead1d6a6dd3d5d4b2eee155f0
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2023-07-06 01:03:06 +0530

    remove firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { FaNodeJs } from "react-icons/fa"
 import HomePage from "./HomePage"
+import * as fs from "fs";
 
 async function getData() {
-  const DB_URL = process.env.databaseURL + '/.json'
-  const res = await fetch(DB_URL, { cache: 'no-store' })
-  const data = res.json()
-  return data
+  // const DB_URL = process.env.databaseURL + '/.json'
+  // const res = await fetch(DB_URL, { cache: 'no-store' })
+  // const data = res.json()
+  try{
+    const jsonData = fs.readFileSync("data.json", "utf-8");
+    console.log("Data from file :  " + jsonData);
+    return JSON.parse(jsonData);
+  } catch (err) {
+    console.log("Error reading file :  " + err);
+  }
 }
-
+ 
 export default async function page() {
 
   const data = await getData()
